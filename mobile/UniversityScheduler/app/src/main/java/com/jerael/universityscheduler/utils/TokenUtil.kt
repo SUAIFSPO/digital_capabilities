5e508fc7a789f712bd259e83eb0464d5dcf75cb2
Vladimir <l.kroyan43@gmail.com>
2021-08-04 11:21:19 +0300

    Создание авторизации

---
+++ mobile/UniversityScheduler/app/src/main/java/com/jerael/universityscheduler/utils/TokenUtil.kt
@@ -0,0 +1,24 @@
+package com.jerael.universityscheduler.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+private const val TOKEN = "TOKEN"
+
+class TokenUtil {
+
+    companion object {
+
+        fun getToken(context: Context): String? {
+            val prefs: SharedPreferences = context.getSharedPreferences(TOKEN, Context.MODE_PRIVATE)
+            return prefs.getString(TOKEN, null)
+        }
+
+        fun setToken(context: Context, newToken: String) {
+            val prefs: SharedPreferences = context.getSharedPreferences(TOKEN, Context.MODE_PRIVATE)
+            prefs.edit().putString(TOKEN, newToken).apply()
+        }
+
+    }
+
+}
