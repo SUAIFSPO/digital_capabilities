bb3c947dd3171c93540677c2c5e770cfafd9e104
Artem <frotiecoder@gmail.com>
2021-08-04 09:20:51 +0300

    Init, NuGuets packages, standart classes

---
+++ api/DatabaseAPI/Models/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseAPI.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+            if(Database.EnsureCreated())
+            {
+                Init();
+            }    
+        }
+
+        private void Init()
+        {
+
+        }
+    }
+}
